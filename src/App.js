8ced4bd780c7d0ece16f168e765594b39da4ab97
Gregory Caloin <gregchapauli@gmail.com>
2022-04-01 15:14:34 +0200

    Mise en place des Route en version 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Docs from './components/Docs';
-import Tutorials from './components/Tutorials';
-import Community from './components/Community';
-import Menu from './components/Menu';
-import ErrorPage from './components/ErrorPage';
-
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Docs from "./components/Docs";
+import Tutorials from "./components/Tutorials";
+import Community from "./components/Community";
+import Menu from "./components/Menu";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Menu />
 
       <Switch>
-        <Route exact path="/" component={Docs} />
-        <Route path="/tutorial" component={Tutorials} />
-        <Route path="/community" component={Community} />
+        <Route exact path="/">
+          <Redirect to="/docs" />
+        </Route>
+
+        <Route exact path="/docs" component={Docs} />
+
+        <Route path="/tutorial">
+          <Tutorials />
+        </Route>
+
+        <Route path="/community" render={() => <Community />} />
+
         <Route component={ErrorPage} />
       </Switch>
-      
-    </BrowserRouter>
+    </>
   );
 }
 
