7c3ddffdad9310745cb5e50832ea1bb2a6f43471
Gregory Caloin <gregchapauli@gmail.com>
2022-04-03 16:31:44 +0200

    Navigate à la place de Redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes, Redirect } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Docs from "./components/Docs";
 import Tutorials from "./components/Tutorials";
 import Community from "./components/Community";
@@ -12,11 +12,9 @@ function App() {
       <Menu />
 
       <Routes>
-        {/* <Route exact path="/">
-          <Redirect to="/docs" />
-        </Route> */}
+        <Route path="/" element={<Navigate to="/docs" replace />} />
 
-        <Route path="/" element={<Docs />} />
+        <Route path="/docs" element={<Docs />} />
 
         <Route path="/tutorial" element={<Tutorials />} />
 
