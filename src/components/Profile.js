ac75870796ad641e36fa1f39da92d2867625ff2c
Gregory Caloin <gregchapauli@gmail.com>
2022-04-04 10:04:17 +0200

    Récupération des informations de profil via API

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Profile = () => {
+  const [data, setData] = useState({});
   const params = useParams();
+
+  useEffect(() => {
+    let payload = {
+      token: "Qf9LqFB_RM_F6B_RRB1fKA",
+      data: {
+        name: "nameFirst",
+        email: "internetEmail",
+        phone: "phoneHome",
+        _repeat: 10,
+      },
+    };
+
+    axios({
+      method: "post",
+      url: "https://app.fakejson.com/q",
+      data: payload,
+    }).then((resp) => {
+      setData(resp.data[params.profilId]);
+    });
+  }, [params.profilId]);
+
+  const { email, name, phone } = data;
 
   return (
     <ul className="list-group list-group-flush">
-      <li className="list-group-item">ID utilisateur {params.profilId}</li>
+      <li className="list-group-item">
+        <strong>Nom:</strong> {name}
+      </li>
+      <li className="list-group-item">
+        <strong>Email:</strong> {email}
+      </li>
+      <li className="list-group-item">
+        <strong>Tel:</strong> {phone}
+      </li>
     </ul>
   );
 };
