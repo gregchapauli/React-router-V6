2d666093a65ce4a53ce7c3565a7dc5637bb6fc0d
Gregory Caloin <gregchapauli@gmail.com>
2022-04-04 10:29:14 +0200

    Paramètres de requêtes

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const Users = () => {
+  const users = ["roger", "stan", "francine", "steve", "hayley", "klaus"];
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchName = searchParams.get("name") || "";
+
+  const handleSearch = (e) => {
+    const name = e.target.value;
+    name ? setSearchParams({ name }) : setSearchParams({});
+  };
+
+  const filteredUsers = users.filter((user) => user.includes(searchName));
+
   return (
     <div className="container mt-3">
       <h1>Profil</h1>
       <Outlet />
+      <hr />
+      <input type="text" value={searchName} onChange={handleSearch} />
+      <ul>
+        {filteredUsers.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
     </div>
   );
 };
